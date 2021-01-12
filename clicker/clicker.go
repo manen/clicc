fcc7d2c931b6c6c3e326caa674bef34def8f709c
manen <varga.bercel.i@gmail.com>
2021-01-12 21:24:20 +0100

    clicker.enabled state

--- clicker/clicker.go
+++ clicker/clicker.go
@@ -2,10 +2,27 @@ package clicker
 
 import "fmt"
 
+// enabled is whether Clicc is enabled
+var enabled bool = false
+
 // Switch switches the clicker
 //
 // So if it was turned on, it turns off, and
 // if it was turned off, it turns on
 func Switch() {
-	fmt.Println("Switched")
+	if enabled {
+		disable()
+	} else {
+		enable()
+	}
+	enabled = !enabled
+}
+
+// disable disables Clicc
+func disable() {
+	fmt.Println("Disabled")
+}
+
+func enable() {
+	fmt.Println("Enabled")
 }
