42cf8eac51f4880b55501c27bc5b21f6a9c33c46
manen <varga.bercel.i@gmail.com>
2021-01-12 21:14:56 +0100

    Nice clicker API

---
+++ clicker/clicker.go
@@ -0,0 +1,11 @@
+package clicker
+
+import "fmt"
+
+// Switch switches the clicker
+//
+// So if it was turned on, it turns off, and
+// if it was turned off, it turns on
+func Switch() {
+	fmt.Println("Switched")
+}
